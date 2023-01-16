97268341460d6bf55be62b3d522f4344c47ac88a
270004270young1 <youngminghei@gmail.com>
2023-01-16 17:19:14 +0800

    Update

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -5,38 +5,34 @@ import {
   HttpLink,
   ApolloLink,
   concat,
-} from "@apollo/client"
-import App from './App'
+} from "@apollo/client";
+import App from "./App";
 
-const httpLink = new HttpLink({uri:'https://hapshare.herokuapp.com/'})
+const httpLink = new HttpLink({
+  uri: "https://hapshare2-server-production.up.railway.app/",
+});
 
-const authMiddleware = new ApolloLink((operation,forward)=>{
-  operation.setContext(({headers = {}})=>({
-    headers:{
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
       ...headers,
-      authorization:localStorage.getItem('jwtToken') || null,
-    }
-  }))
-  return forward(operation)
-})
+      authorization: localStorage.getItem("jwtToken") || null,
+    },
+  }));
+  return forward(operation);
+});
 
 const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
+});
 
-  
-  cache:new InMemoryCache(),
-  link:concat(authMiddleware,httpLink)
-  
-})
-
-
-function ApolloSetup(){
-    return(
-        <ApolloProvider client={client}>
-        
-            <App/>
-        
-        </ApolloProvider>
-    )
+function ApolloSetup() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
 }
 
-export default ApolloSetup
+export default ApolloSetup;
